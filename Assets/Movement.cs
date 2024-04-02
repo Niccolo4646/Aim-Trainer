96c997f038391a8096ec9a666e8753e4e4b72116
Niccolo4646 <nicolodaniello50@gmail.com>
2024-04-02 14:34:46 +0200

    Camera and player movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    /* Movimento */
+    Rigidbody rb;
+    Vector3 directon;
+    [SerializeField] int currentSpeed;
+
+    /* Salto */
+    bool isGrounded;
+    [SerializeField] float jumpForce;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        directon = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        directon = transform.TransformDirection(directon);
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + directon * currentSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
+    }
+}
